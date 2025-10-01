eed6a8f055628a083ef10a8a34d4cb564807c4fe
Uber830 <uber.cardoso@uniminuto.edu.co>
2025-10-01 01:56:38 -0500

    feat: ✨ enhance project setup with package-lock and morgan integration
    - Added package-lock.json for dependency management.
    - Updated .gitignore to include temporary files and cursor settings.
    - Integrated morgan.
    - Expanded package.json scripts for Prisma database management.
    - Improved organization of customer and service routes for better clarity.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import express from "express";
 //import adminRoutes from './modules/admin/routes';
 import { errorHandler } from "./core/middleware/error-handler";
 import { logger } from "./core/logger";
+import morgan from "morgan";
 import cors from "cors";
 import { validationErrorHandler } from "./core/middleware/validation-error-handler";
 import { authMiddleware } from "./modules/auth/middleware/auth";
@@ -10,10 +11,14 @@ import { authMiddleware } from "./modules/auth/middleware/auth";
 import authRoutes from "./modules/auth/routes";
 import workshopRoutes from "./modules/workshop/workshop.routes";
 import { userRoutes } from "./modules/user";
+import { serviceRoutes } from "./modules/services";
+import { workOrderRoutes } from "./modules/work-orders";
+import { supplyRoutes } from "./modules/supplies";
 import customerRoutes from "./modules/customer/customer.routes";
 
 const app = express();
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(
   cors({
     origin: "*",
@@ -21,14 +26,18 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-
+// Routes without authentication required
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 app.use("/api", authMiddleware);
 
-app.use("/api/workshops", workshopRoutes);
-app.use("/api/customers", customerRoutes);
+// Protected routes
+app.use("/api/workshops", authMiddleware, workshopRoutes);
+app.use("/api/customers", authMiddleware, customerRoutes);
+app.use("/api/services", authMiddleware, serviceRoutes);
+app.use("/api/work-orders", authMiddleware, workOrderRoutes);
+app.use("/api/supplies", authMiddleware, supplyRoutes);
 
 // Admin routes
 //app.use('/api/admin', adminMiddleware, adminRoutes);
